0334cde4a0ffe7f7d3f1f02cc044205a993dea3b
Meet Rajesh Gor <40317114+Mr-Destructive@users.noreply.github.com>
2023-02-26 18:56:06 +0530

    add searchable fields for django admin

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
-from .models import Organization, Person, Grant, Funder, Publication, Dataset, Asset
+from .models import Organization, Funder, Person, Grant, Publication, Dataset, Asset
 
-admin.site.register(Organization)
-admin.site.register(Person)
-admin.site.register(Grant)
-admin.site.register(Funder)
-admin.site.register(Publication)
-admin.site.register(Dataset)
-admin.site.register(Asset)
+
+class OrganizationAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name',]
+
+
+class FunderAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name',]
+
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'first_name', 'last_name', 'emails',]
+
+
+class GrantAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'title',]
+
+
+class PublicationAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'title']
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'title']
+
+
+class AssetAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'filename']
+    
+ 
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Funder, FunderAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Grant, GrantAdmin)
+admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(Asset, AssetAdmin)
