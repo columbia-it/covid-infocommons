3e4b3f6a9b875fa16117c5a06b85633536f54dfa
sg3847 <sg3847@columbia.edu>
2023-02-28 13:58:19 -0500

    Allow searchand filter in Django admin for all models, except Funder

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 from .models import Organization, Person, Grant, Funder, Publication, Dataset, Asset
 
+
+"""
+ModelAdmin class to customize Person model view in Django admin
+"""
+class PersonAdmin(admin.ModelAdmin):
+    # Add filtering on fields
+    list_filter = ('first_name', 'last_name',)
+    # Add search on fields
+    search_fields = ('first_name', 'id', 'last_name', 'emails',)
+
+
+"""
+ModelAdmin class to customize Grant model view in Django admin
+"""
+class GrantAdmin(admin.ModelAdmin):
+    # Add filtering on fields
+    list_filter = ('principal_investigator__first_name', 'principal_investigator__last_name',)
+    # Add search on fields
+    search_fields = ('title', 'award_id', 'principal_investigator__first_name', 'principal_investigator__last_name')
+    # Show the fields as inputs instead of drop-down lists to avoid overhead
+    raw_id_fields = ('principal_investigator', 'awardee_organization', 'program_officials', 'other_investigators', 'funder')
+
+
 admin.site.register(Organization)
-admin.site.register(Person)
-admin.site.register(Grant)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Grant, GrantAdmin)
 admin.site.register(Funder)
 admin.site.register(Publication)
 admin.site.register(Dataset)
