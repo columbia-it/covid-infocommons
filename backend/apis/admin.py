a1ef09683f2792b2fa881a8123e25b6952a3f648
sg3847 <sg3847@columbia.edu>
2023-02-28 13:58:30 -0500

    Allow searchand filter in Django admin for all models, except Funder

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -24,10 +24,42 @@ class GrantAdmin(admin.ModelAdmin):
     raw_id_fields = ('principal_investigator', 'awardee_organization', 'program_officials', 'other_investigators', 'funder')
 
 
-admin.site.register(Organization)
+"""
+ModelAdmin class to customize Organization model view in Django admin
+"""
+class OrganizationAdmin(admin.ModelAdmin):
+    # Add search on fields
+    search_fields = ('name', 'id', 'city', 'state',)
+
+
+"""
+ModelAdmin class to customize Publication model view in Django admin
+"""
+class PublicationAdmin(admin.ModelAdmin):
+    # Add search on fields
+    search_fields = ('doi', 'id', 'title', 'grants__id',)
+
+
+"""
+ModelAdmin class to customize Dataset model view in Django admin
+"""
+class DatasetAdmin(admin.ModelAdmin):
+    # Add search on fields
+    search_fields = ('doi', 'id', 'title', 'grants__id',)
+
+
+"""
+ModelAdmin class to customize Asset model view in Django admin
+"""
+class AssetAdmin(admin.ModelAdmin):
+    # Add search on fields
+    search_fields = ('doi', 'id', 'author__id', 'grants__id', 'publication__id',)
+
+
+admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Grant, GrantAdmin)
 admin.site.register(Funder)
-admin.site.register(Publication)
-admin.site.register(Dataset)
-admin.site.register(Asset)
+admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(Asset, AssetAdmin)
