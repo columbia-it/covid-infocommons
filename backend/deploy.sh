7257f0c53c9d03590aea36e9f868ffcb6c4e235e
sg3847 <sg3847@columbia.edu>
2023-06-07 14:10:48 -0400

    Run tests during auto deployment

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,63 +1,28 @@
-#!/bin/sh
-
+#!/bin/bash 
 date +"%Y/%m/%d %H:%M:%S"
 
-which python3
-
 export CICE_ENV=dev
-export AWS_NEW_CRED=yes
+export AWS_NEW_CRED=Yes
 
-if [ "$CICE_ENV" == "dev" ]
-then
-    export ACS_AWS_ACCOUNT=305803678806
-    export AWS_PROFILE=cuit-infra-cice-dev
-fi
-
-if [ "$CICE_ENV" == "prod" ]
-then
-    export ACS_AWS_ACCOUNT=031752658700
-    export AWS_PROFILE=cuit-infra-cice-prod
-fi
-
-if [ -d "py3_venv" ]
-then
-    echo virtual env exists. deleting existing virtual env.
-    rm -r py3_venv/
-fi
-
-git checkout main
-git pull
-
+which python3
+python3 -V
 python3 -m venv py3_venv
 source py3_venv/bin/activate
+
+pwd
 pip install -r requirements.txt
-pip freeze
+python manage.py test --pattern="test_*.py"
 
-###
-# Enable the code below for auth once we have the AWS key ID and secret.
-# Use the workaround untill then.
-###
-
-# if [ $AWS_NEW_CRED == "yes" ]
-# then
-#   echo "Generating a new AWS credentials"
-#   python create_aws_cred.py
-# fi
-
-## Workaround 
-export PATH=$PATH:/Users/sg3847/Downloads
-python saml.py cuit-dev-role 305803678806
-aws sts get-caller-identity
-
-python manage.py collectstatic
+#python manage.py collectstatic --noinput
+if [ $AWS_NEW_CRED == "Yes" ]
+then
+  echo "Generating a new AWS credentials"
+  python create_aws_cred.py
+fi
+ls -al ~/.aws/
 
 zappa status $CICE_ENV
 zappa update $CICE_ENV
-zappa manage $CICE_ENV showmigrations
-zappa manage $CICE_ENV migrate
-zappa manage $CICE_ENV showmigrations
-#zappa manage $CICE_ENV "collectstatic --noinput"
 
 date +"%Y/%m/%d %H:%M:%S"
 
-deactivate
