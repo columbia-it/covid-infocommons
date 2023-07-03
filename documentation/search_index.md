03cd0638b5d638f068248247e8f430b52602b4d5
Ryan Scherle <ryan@scherle.org>
2023-07-03 11:01:48 -0400

    More documentation about working with the search index

--- documentation/search_index.md
+++ documentation/search_index.md
@@ -12,4 +12,20 @@ environment.
 
 Hostname should be set to the search cluster's Domain endpoint (VPC).
 
+All commands must be run through the bash shell at the bottom of Cloud9.
+
+First-time setup:
+- create a Cloud9 environment in the CIC AWS account
+- the environment must use an SSH connection
+- run the environment, and in the bash window at the bottom, run:
+```
+python -m venv venv
+pip install opensearch-py
+```
+
+Running code:
+```
+source venv/bin/activate
+python search.py 
+```
 
---
+++ documentation/search_index/search.py
@@ -0,0 +1,24 @@
+from opensearchpy import OpenSearch
+import json
+
+def main():
+    host = '<VPC_DOMAIN_ENDPOINT>'
+
+    client = OpenSearch(
+        hosts = [{'host': host, 'port': 443}],
+        # http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+    )
+    query_body = {
+      "query": {
+            "match_all" :  {}
+        }
+     }
+    
+
+    response = client.search(index="grant_index", body=query_body)
+    print(response['hits'])
+
+if __name__=="__main__":
+    main()
