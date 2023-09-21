b3bfd1b3259bebbc4f21357d13307b0a4b2fbc28
Ryan Scherle <ryan@scherle.org>
2023-09-21 18:07:42 +0000

    update API metadata

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'COVID Info Commons (CIC) API'
 __version__ = '1.0.0'
-__author__ = 'Sunaina Gupta'
-__copyright__ = '(c) 2018-2022 The Trustees of Columbia University in the City of New York'
+__author__ = 'Sunaina Gupta, Ryan Scherle'
+__copyright__ = '(c) 2018-2023 The Trustees of Columbia University in the City of New York'
 
 # Version synonym
 VERSION = __version__
