98e89c39b038d9e1eabab9ea35ed98f61d15005e
Ryan Scherle <ryan@scherle.org>
2025-12-07 13:28:03 +0000

    update bold

--- frontend/search/src/grants.tsx
+++ frontend/search/src/grants.tsx
@@ -483,8 +483,8 @@ class App extends Component<any, AppState> {
         {
         this.state.search_in_progress == false ? 
         <div className='results-row'>
-		Showing <span style={{fontWeight: 'bold', color: '#000000'}}>{ this.state.totalCount }</span> results for COVID-related grants.
-		To search over other types of content, visit the <a href="/adv_search">Advanced Search</a> page.
+		Showing <span style={{fontWeight: 'bold', color: '#000000'}}>{ this.state.totalCount }</span> results for COVID-related grants.<br/><br/>
+		<b>Visit the <a href="/adv_search">Advanced Search page</a> to search over other types of content.</b>
         </div> 
         : <div className='results-row'>Waiting for results...
         </div> 
