d9c3f2caaaa88eb4b1f0f2b7c516cc977806e1b9
Ryan Scherle <ryan@scherle.org>
2025-12-14 22:23:53 +0000

    improve display of advanced search link

--- frontend/search/src/grants.tsx
+++ frontend/search/src/grants.tsx
@@ -484,7 +484,7 @@ class App extends Component<any, AppState> {
         this.state.search_in_progress == false ? 
         <div className='results-row'>
 		Showing <span style={{fontWeight: 'bold', color: '#000000'}}>{ this.state.totalCount }</span> results for COVID-related grants.<br/><br/>
-		<b>Visit the <a href="/adv_search">Advanced Search page</a> to find datasets and publications related to your keyword search.</b>
+		<span style={{backgroundColor: '#D8D8D8', padding: '3px'}}><b>Visit the <a href="/adv_search">Advanced Search page</a> to find datasets and publications related to your keyword search.</b></span>
         </div> 
         : <div className='results-row'>Waiting for results...
         </div> 
